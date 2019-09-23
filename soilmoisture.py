9040d587dcd788e817329f0a6ba0ec076544c021
larfan <victor.lindenbauer@gmx.at>
2019-09-23 17:05:45 +0200

    nocheinmal soilmoisture änderung

--- soilmoisture.py
+++ soilmoisture.py
@@ -10,9 +10,6 @@ spi.open(0,0)
 led_pin = 20
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(led_pin, GPIO.OUT)
-# Creating a PWM channel at 100Hz frequency
-pwm = GPIO.PWM(led_pin, 100)
-pwm.start(0) 
 # Read MCP3008 data
 def analogInput(channel):
   spi.max_speed_hz = 1350000
@@ -23,5 +20,4 @@ while True:
   output = analogInput(0) # Reading from CH0
   output = interp(output, [0, 1023], [0, 100])
   print(output)
-  pwm.ChangeDutyCycle(output)
   sleep(0.1)
