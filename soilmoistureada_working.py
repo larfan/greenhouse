8b224719ea0ba887b60a4db3b77b9c16f0845b0c
larfan <victor.lindenbauer@gmx.at>
2019-09-24 18:20:42 +0200

    median

--- soilmoistureada_working.py
+++ soilmoistureada_working.py
@@ -18,13 +18,14 @@ print('-' * 57)
 # Main program loop.
 while True:
     # Read all the ADC channel values in a list.
-    
-    values = [0]*8  ##dass man sozusagen eine list macht
-    for i in range(8):
-        # The read_adc function will get the value of the specified channel (0-7).
-        values[i] = mcp.read_adc(i)
-    for i in range(2):
-        data.append(values[i])
+    data=[]
+    for i in range(5):
+          ##dass man sozusagen eine list macht
+        for i in range(8):
+              # The read_adc function will get the value of the specified channel (0-7).
+            values[i] = mcp.read_adc(i)
+        for i in range(2):
+            data.append(values[i])
     # Print the ADC values.
     print(data)
     print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*values))
