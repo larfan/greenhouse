08471ad904f2a42c4e6349878ee691abb8626a07
larfan <victor.lindenbauer@gmx.at>
2019-09-24 19:38:28 +0200

    erstes mal zum raspi

---
+++ cleanprograms/lightsensors.py
@@ -0,0 +1,35 @@
+# Simple example of reading the MCP3008 analog input channels and printing
+import time
+import statistics
+# Import SPI library (for hardware SPI) and MCP3008 library.
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_MCP3008
+import numpy
+
+#Hardware SPI configuration: ##das ist allgemein für MCP3008
+SPI_PORT   = 0
+SPI_DEVICE = 0
+mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
+
+
+def lightsensor():
+    # Read all the ADC channel values in a list.
+    data=[]
+    values = [0]*8
+    for i in range(5):
+        for i in range(8):
+              # The read_adc function will get the value of the specified channel (0-7).
+            values[i] = mcp.read_adc(i)
+        for i in range(2):
+            data.append(values[i])
+        time.sleep(0.5)
+    # Print the ADC values.
+    x=statistics.mean(data)
+    x=interp(x, [0, 1023], [0, 100])
+    print(data)
+    print(x)
+    print('\n')
+    # Pause for half a second.
+    time.sleep(2)
+
+lightsensor()
