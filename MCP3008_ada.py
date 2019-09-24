02f9fac80b66598a9179d472bd9c304cd40e9491
larfan <victor.lindenbauer@gmx.at>
2019-09-24 18:43:49 +0200

    df

--- MCP3008_ada.py
+++ MCP3008_ada.py
@@ -27,11 +27,13 @@ while True:
             values[i] = mcp.read_adc(i)
         for i in range(2):
             data.append(values[i])
+        time.sleep(0.5)
     # Print the ADC values.
     x=statistics.mean(data)
     print(data)
     print(x)
     print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*values))
+    print('\n')
     # Pause for half a second.
-    time.sleep(0.5)
+    time.sleep(2)
 
