5478a21f316d1ff43767d79f92c75b55b632a2fc
larfan <victor.lindenbauer@gmx.at>
2019-10-05 23:54:49 +0200

    rewrew

--- co2japanreduziert.py
+++ co2japanreduziert.py
@@ -3,7 +3,7 @@ import time
 import subprocess
 import traceback
 import getrpimodel
-
+import struct
 import platform
 import sys
 
@@ -38,11 +38,17 @@ def mh_z19():
     while 1:
       result=ser.write(b"\xff\x01\x86\x00\x00\x00\x00\x00\x79")
       s=ser.read(9)
-    if len(s) >= 4 and s[0] == 0xff and s[1] == 0x86:
-        return {'co2': s[2]*256 + s[3]}
-        print('testus')
+
+      if p_ver == '2':
+        if len(s) >= 4 and s[0] == "\xff" and s[1] == "\x86":
+          return {'co2': ord(s[2])*256 + ord(s[3])}
+        break
+      else:
+        if len(s) >= 4 and s[0] == 0xff and s[1] == 0x86:
+          return {'co2': s[2]*256 + s[3]}
+        break
   except:
-     traceback.print_exc()      #zeigt wo problem ist
+     traceback.print_exc()
 
 def read():
   p = subprocess.call(stop_getty, stdout=subprocess.PIPE, shell=True)
@@ -56,7 +62,8 @@ def read():
 
 
 
-
+def checksum(array):
+  return struct.pack('B', 0xff - (sum(array) % 0x100) + 1)
 
 if __name__ == '__main__':
     value = read()
