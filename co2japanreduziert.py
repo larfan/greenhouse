36aa1d2335f61228cad8152fb624765a8c576a40
larfan <victor.lindenbauer@gmx.at>
2019-10-06 00:01:52 +0200

    df

--- co2japanreduziert.py
+++ co2japanreduziert.py
@@ -38,9 +38,7 @@ def mh_z19():
     while 1:
       result=ser.write(b"\xff\x01\x86\x00\x00\x00\x00\x00\x79")
       s=ser.read(9)
-
-     
-    if len(s) >= 4 and s[0] == 0xff and s[1] == 0x86:
+      if len(s) >= 4 and s[0] == 0xff and s[1] == 0x86:
         return {'co2': s[2]*256 + s[3]}
     
   except:
