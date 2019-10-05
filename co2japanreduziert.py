2d4701de1988dcddeefc6df62f2c8572850a9c5b
larfan <victor.lindenbauer@gmx.at>
2019-10-06 00:18:55 +0200

    dfd

--- co2japanreduziert.py
+++ co2japanreduziert.py
@@ -2,7 +2,6 @@ import serial
 import time
 import subprocess
 import traceback
-import getrpimodel
 import struct
 import platform
 import sys
@@ -17,8 +16,6 @@ serial_dev = '/dev/%s' % partial_serial_dev
 stop_getty = 'sudo systemctl stop serial-getty@%s.service' % partial_serial_dev
 start_getty = 'sudo systemctl start serial-getty@%s.service' % partial_serial_dev
 
-# major version of running python
-p_ver = 3
 
 def set_serialdevice(serialdevicename):
   global serial_dev
@@ -34,14 +31,15 @@ def connect_serial():
 
 def mh_z19():
   try:
+    p = subprocess.call(stop_getty, stdout=subprocess.PIPE, shell=True)
     ser = connect_serial()
     result=ser.write(b"\xff\x01\x86\x00\x00\x00\x00\x00\x79")
     s=ser.read(9)
     if len(s) >= 4 and s[0] == 0xff and s[1] == 0x86:
         return {'co2': s[2]*256 + s[3]}
-    
+    p = subprocess.call(start_getty, stdout=subprocess.PIPE, shell=True)
   except:
-     traceback.print_exc()
+    traceback.print_exc()
 
 def read():
   p = subprocess.call(stop_getty, stdout=subprocess.PIPE, shell=True)
@@ -52,14 +50,10 @@ def read():
 
 
 
-
-
-
-
 if __name__ == '__main__':
     value = read()
     print('test')
-    print(value)
+    print(mh_z19())
     
 
 sys.exit(0)
