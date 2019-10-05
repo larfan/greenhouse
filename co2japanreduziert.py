896caad243e3e9b9bd4f51f3662d76f29045dc6d
larfan <victor.lindenbauer@gmx.at>
2019-10-05 19:59:50 +0200

    df

--- co2japanreduziert.py
+++ co2japanreduziert.py
@@ -55,67 +55,24 @@ def mh_z19():
   except:
      traceback.print_exc()
 
-
-def abc_on():
+def read():
   p = subprocess.call(stop_getty, stdout=subprocess.PIPE, shell=True)
-  ser = connect_serial()
-  result=ser.write(b"\xff\x01\x79\xa0\x00\x00\x00\x00\xe6")
-  ser.close()
+  result = mh_z19()
   p = subprocess.call(start_getty, stdout=subprocess.PIPE, shell=True)
+  if result is not None:
+    return result
 
-def abc_off():
-  p = subprocess.call(stop_getty, stdout=subprocess.PIPE, shell=True)
-  ser = connect_serial()
-  result=ser.write(b"\xff\x01\x79\x00\x00\x00\x00\x00\x86")
-  ser.close()
-  p = subprocess.call(start_getty, stdout=subprocess.PIPE, shell=True)
 
-def span_point_calibration(span):
-  p = subprocess.call(stop_getty, stdout=subprocess.PIPE, shell=True)
-  ser = connect_serial()
-  if p_ver == '2':
-    b3 = span / 256;
-  else:
-    b3 = span // 256;   
-  byte3 = struct.pack('B', b3)
-  b4 = span % 256; byte4 = struct.pack('B', b4)
-  c = checksum([0x01, 0x88, b3, b4])
-  request = b"\xff\x01\x88" + byte3 + byte4 + b"\x00\x00\x00" + c
-  result = ser.write(request)
-  ser.close()
-  p = subprocess.call(start_getty, stdout=subprocess.PIPE, shell=True)
 
-def zero_point_calibration():
-  p = subprocess.call(stop_getty, stdout=subprocess.PIPE, shell=True)
-  ser = connect_serial()
-  request = b"\xff\x01\x87\x00\x00\x00\x00\x00\x78"
-  result = ser.write(request)
-  ser.close()
-  p = subprocess.call(start_getty, stdout=subprocess.PIPE, shell=True)
 
-def detection_range_5000():
-  p = subprocess.call(stop_getty, stdout=subprocess.PIPE, shell=True)
-  ser = connect_serial()
-  request = b"\xff\x01\x99\x00\x00\x00\x13\x88\xcb"
-  result = ser.write(request)
-  ser.close()
-  p = subprocess.call(start_getty, stdout=subprocess.PIPE, shell=True)
 
-def detection_range_2000():
-  p = subprocess.call(stop_getty, stdout=subprocess.PIPE, shell=True)
-  ser = connect_serial()
-  request = b"\xff\x01\x99\x00\x00\x00\x07\xd0\x8F"
-  result = ser.write(request)
-  ser.close()
-  p = subprocess.call(start_getty, stdout=subprocess.PIPE, shell=True)
 
 def checksum(array):
   return struct.pack('B', 0xff - (sum(array) % 0x100) + 1)
 
 if __name__ == '__main__':
-    if mh_z19() is not None:
-        value= mh_z19()
-        print('test')
-        print (json.dumps(value))
+    value = read()
+    print('test')
+    print (json.dumps(value))
 
 sys.exit(0)
