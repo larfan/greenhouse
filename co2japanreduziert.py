31e45f7133571ba5c28877bff392788f33fa35b9
larfan <victor.lindenbauer@gmx.at>
2019-10-06 18:48:55 +0200

    dfdsaf

--- co2japanreduziert.py
+++ co2japanreduziert.py
@@ -5,28 +5,24 @@ import traceback
 import platform
 import sys
 
+partial_serial_dev = 'serial0'
 
+serial_dev = '/dev/%s' % partial_serial_dev
+stop_getty = 'sudo systemctl stop serial-getty@%s.service' % partial_serial_dev
+start_getty = 'sudo systemctl start serial-getty@%s.service' % partial_serial_dev
 
-
-
-
-def mh_z19():
-  partial_serial_dev = 'serial0'
-
-  serial_dev = '/dev/%s' % partial_serial_dev
-  stop_getty = 'sudo systemctl stop serial-getty@%s.service' % partial_serial_dev
-  start_getty = 'sudo systemctl start serial-getty@%s.service' % partial_serial_dev
-  def connect_serial():
-    return serial.Serial(serial_dev,
+serial_settings= serial.Serial(serial_dev,
                         baudrate=9600,
                         bytesize=serial.EIGHTBITS,
                         parity=serial.PARITY_NONE,
                         stopbits=serial.STOPBITS_ONE,
                         timeout=1.0)
-  
+
+
+def mh_z19():
   try:
     p = subprocess.call(stop_getty, stdout=subprocess.PIPE, shell=True)
-    ser = connect_serial()
+    ser = serial_settings
     result=ser.write(b"\xff\x01\x86\x00\x00\x00\x00\x00\x79")
     s=ser.read(9)
     if len(s) >= 4 and s[0] == 0xff and s[1] == 0x86:
