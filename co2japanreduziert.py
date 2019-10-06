d11ad09bd56251e632b94d54768985f39ccff2ab
larfan <victor.lindenbauer@gmx.at>
2019-10-06 14:36:40 +0200

    dfdf

--- co2japanreduziert.py
+++ co2japanreduziert.py
@@ -17,10 +17,6 @@ stop_getty = 'sudo systemctl stop serial-getty@%s.service' % partial_serial_dev
 start_getty = 'sudo systemctl start serial-getty@%s.service' % partial_serial_dev
 
 
-def set_serialdevice(serialdevicename):
-  global serial_dev
-  serial_dev = serialdevicename
-
 def connect_serial():
   return serial.Serial(serial_dev,
                         baudrate=9600,
@@ -36,17 +32,15 @@ def mh_z19():
     result=ser.write(b"\xff\x01\x86\x00\x00\x00\x00\x00\x79")
     s=ser.read(9)
     if len(s) >= 4 and s[0] == 0xff and s[1] == 0x86:
-        return {'co2': s[2]*256 + s[3]}
+        value= {'co2': s[2]*256 + s[3]}
+        if value is not None:
+            return value
+        else:
+            print('kein wert')
     p = subprocess.call(start_getty, stdout=subprocess.PIPE, shell=True)
   except:
     traceback.print_exc()
 
-def read():
-  p = subprocess.call(stop_getty, stdout=subprocess.PIPE, shell=True)
-  result = mh_z19()
-  p = subprocess.call(start_getty, stdout=subprocess.PIPE, shell=True)
-  if result is not None:
-    return result
 
 
 
