e31d8b515334e25431c8ef1535d7cb265ab3fce1
larfan <victor.lindenbauer@gmx.at>
2019-10-06 17:23:32 +0200

    dfdsf

--- co2japanreduziert.py
+++ co2japanreduziert.py
@@ -11,6 +11,8 @@ import sys
 
 
 
+
+
 def connect_serial():
   return serial.Serial(serial_dev,
                         baudrate=9600,
@@ -18,14 +20,20 @@ def connect_serial():
                         parity=serial.PARITY_NONE,
                         stopbits=serial.STOPBITS_ONE,
                         timeout=1.0)
-
 def mh_z19():
   partial_serial_dev = 'serial0'
 
   serial_dev = '/dev/%s' % partial_serial_dev
   stop_getty = 'sudo systemctl stop serial-getty@%s.service' % partial_serial_dev
   start_getty = 'sudo systemctl start serial-getty@%s.service' % partial_serial_dev
-
+  def connect_serial():
+    return serial.Serial(serial_dev,
+                        baudrate=9600,
+                        bytesize=serial.EIGHTBITS,
+                        parity=serial.PARITY_NONE,
+                        stopbits=serial.STOPBITS_ONE,
+                        timeout=1.0)
+  
   try:
     p = subprocess.call(stop_getty, stdout=subprocess.PIPE, shell=True)
     ser = connect_serial()
