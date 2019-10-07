cf947336ab3414b6f4c196aff591a500e5ef5165
larfan <victor.lindenbauer@gmx.at>
2019-10-07 17:12:04 +0200

    sdfsa

--- sensor2echt.py
+++ sensor2echt.py
@@ -3,7 +3,6 @@
 import Adafruit_DHT
 import time
 
-
 humidity, temperature = Adafruit_DHT.read_retry(11, 4)
 
 def Temp_humidity(): 
@@ -12,9 +11,11 @@ def Temp_humidity():
         humidity, temperature = Adafruit_DHT.read_retry(11, 4)
 
         print('Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity))
+        print(test)
         print(temperature)                      #.format ersetzt einfach 
         time.sleep(5)                           #das in den geschwungenen
                                             #Klammern. % ist veraltet
         #read_retry ist eine Funktion von Adafruit. in common.py ist
         #angegeben dass es 15* probiert mit einem abstand von 2 sek
         
+print(Temp_humidity)
