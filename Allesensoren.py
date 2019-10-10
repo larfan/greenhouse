ecaaf4804298c9a1240f6b448c3be1c8fdfd0ea9
larfan <victor.lindenbauer@gmx.at>
2019-10-10 17:44:51 +0200

    dsfdsa

--- Allesensoren.py
+++ Allesensoren.py
@@ -23,6 +23,23 @@ SPI_PORT   = 0
 SPI_DEVICE = 0
 mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 
+#co2sensor
+partial_serial_dev = 'serial0'
+
+serial_dev = '/dev/%s' % partial_serial_dev
+stop_getty = 'sudo systemctl stop serial-getty@%s.service' % partial_serial_dev
+start_getty = 'sudo systemctl start serial-getty@%s.service' % partial_serial_dev
+
+serial_settings= serial.Serial(serial_dev,
+                        baudrate=9600,
+                        bytesize=serial.EIGHTBITS,
+                        parity=serial.PARITY_NONE,
+                        stopbits=serial.STOPBITS_ONE,
+                        timeout=1.0)
+p = subprocess.call(stop_getty, stdout=subprocess.PIPE, shell=True)
+ser = serial_settings
+    
+
 
 
 
@@ -49,6 +66,17 @@ class lightsensors:
       self.data=statistics.mean(self.data)
       self.data=round(interp(self.data, [0, 1023], [0, 100]),2) 
 
+class mh_z19:
+    result=ser.write(b"\xff\x01\x86\x00\x00\x00\x00\x00\x79")
+    s=ser.read(9)   #ich glaube man braucht die variablen nicht
+    value={}        #
+    def werte(self):
+      subprocess.call(stop_getty, stdout=subprocess.PIPE, shell=True)
+      if len(self.s) >= 4 and s[0] == 0xff and s[1] == 0x86:
+        self.value= {'co2': self.s[2]*256 + s[3]}
+      if self.value is None:
+        self.value={'co2 sensor nichts gelesen'}
+      subprocess.call(start_getty, stdout=subprocess.PIPE, shell=True)
 
 while True:
   sensor=dht_11()
@@ -60,5 +88,9 @@ while True:
   lichtsensor=lightsensors()
   lichtsensor.medianlight()
   print(lichtsensor.data)
+  #co2
+  co2sensor=mh_z19()
+  co2sensor.werte()
+  print(co2sensor.value)
 
   time.sleep(1) 
