a8635866f51a680dc90077c2883b93a056c45e9c
larfan <victor.lindenbauer@gmx.at>
2019-10-10 22:40:20 +0200

    dsfa

--- Allesensoren.py
+++ Allesensoren.py
@@ -30,14 +30,16 @@ serial_dev = '/dev/%s' % partial_serial_dev
 stop_getty = 'sudo systemctl stop serial-getty@%s.service' % partial_serial_dev
 start_getty = 'sudo systemctl start serial-getty@%s.service' % partial_serial_dev
 
-serial_settings= serial.Serial(serial_dev,
+
+
+
+def connect_serial():
+  return serial.Serial(serial_dev,
                         baudrate=9600,
                         bytesize=serial.EIGHTBITS,
                         parity=serial.PARITY_NONE,
                         stopbits=serial.STOPBITS_ONE,
                         timeout=1.0)
-p = subprocess.call(stop_getty, stdout=subprocess.PIPE, shell=True)
-ser = serial_settings
     
 
 
@@ -67,19 +69,18 @@ class lightsensors:
       self.data=round(interp(self.data, [0, 1023], [0, 100]),2) 
 
 class mh_z19:
-    result=ser.write(b"\xff\x01\x86\x00\x00\x00\x00\x00\x79")
-    s=ser.read(9)   #ich glaube man braucht die variablen nicht
-    value={}        #
-    def werte(self):
-      
-      ser.write(b"\xff\x01\x86\x00\x00\x00\x00\x00\x79")
-      time.sleep(5)
-      self.s=ser.read(9)
-      
-      if self.value is None:
-        self.value={'co2 sensor nichts gelesen'}
-      elif len(self.s) >= 4 and self.s[0] == 0xff and self.s[1] == 0x86:
-        self.value= {'co2': self.s[2]*256 + self.s[3]}
+  def werte(self):
+    try:
+      ser = connect_serial()
+      while 1:
+        result=ser.write(b"\xff\x01\x86\x00\x00\x00\x00\x00\x79")
+        s=ser.read(9)
+        if len(s) >= 4 and s[0] == 0xff and s[1] == 0x86:
+          return {'co2': s[2]*256 + s[3]}
+          print('1')
+          break
+    except:
+      traceback.print_exc()
       
 
 while True:
@@ -92,10 +93,9 @@ while True:
   lichtsensor.medianlight()
   print(lichtsensor.data)
   #co2
-  co2sensor=mh_z19()
+  co2sensor=mh_z19
   p = subprocess.call(stop_getty, stdout=subprocess.PIPE, shell=True)
-  co2sensor.werte()
+  print(co2sensor.werte())
   p = subprocess.call(start_getty, stdout=subprocess.PIPE, shell=True)
-  print(co2sensor.value)
+  time.sleep(5)
 
-  time.sleep(10) 
