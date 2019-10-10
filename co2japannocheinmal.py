1bf5553eed9bdae7154087048fdeae7ff4efcb01
larfan <victor.lindenbauer@gmx.at>
2019-10-10 20:39:20 +0200

    dsfasjk

--- co2japannocheinmal.py
+++ co2japannocheinmal.py
@@ -36,11 +36,9 @@ def mh_z19():
     while 1:
       result=ser.write(b"\xff\x01\x86\x00\x00\x00\x00\x00\x79")
       s=ser.read(9)
-
-      
-    if len(s) >= 4 and s[0] == 0xff and s[1] == 0x86:
+      if len(s) >= 4 and s[0] == 0xff and s[1] == 0x86:
         return {'co2': s[2]*256 + s[3]}
-    break
+        break
   except:
      traceback.print_exc()##sollte sagen wo ein problem ist wenn eines das ist
 
